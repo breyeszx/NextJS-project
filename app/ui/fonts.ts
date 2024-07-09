4b710e89d5e2fa06b622f99b3801009f8647c1cc
breyeszx <brau.reyes@duocuc.cl>
2024-07-09 01:38:06 -0400

    font lusitana

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,8 @@
-import { Inter, Roboto } from "next/font/google";
+import { Inter, Roboto, Lusitana } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
 export const roboto = Roboto({ subsets: ["latin"], weight: ["400"] });
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
